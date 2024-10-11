1291f4b38b8f437f1fd03c64460139b469a70641
Pablo Grossi García <38209385+PabloGarciaGrossi@users.noreply.github.com>
2024-10-11 18:02:16 +0200

    Lighting master

--- Scripts/LightScripts/Lightning.gd
+++ Scripts/LightScripts/Lightning.gd
@@ -8,8 +8,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var light_positions = _get_light_positions()
-	var radius = _get_light_radius()
+	#var light_positions = _get_light_positions()
+	#var radius = _get_light_radius()
+	var lights = _get_lights()
+	var light_positions = []
+	var radius = []
+	lights.sort_custom(func(a,b): return a.radius > b.radius)
+	
+	for i in range(lights.size()):
+		light_positions.append(lights[i].get_global_transform_with_canvas().origin)
+		radius.append(lights[i].radius)
+	
 	material.set_shader_parameter("number_of_lights", light_positions.size())
 	material.set_shader_parameter("lights", light_positions)
 	
@@ -26,4 +35,10 @@ func _get_light_radius():
 	return get_tree().get_nodes_in_group("light").map(
 		func(light: LightMarker):
 			return light.radius
+	)
+	
+func _get_lights():
+	return get_tree().get_nodes_in_group("light").map(
+		func(light: LightMarker):
+			return light
 	)
